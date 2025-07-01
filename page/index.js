8e6cbcd8589d3390b5754d549562e86954140037
viveka2024 <humbertocoaching@gmail.com>
2025-07-02 00:09:13 +0200

    Delete page directory

--- page/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: '50px', fontFamily: 'sans-serif' }}>
-      <h1>Bienvenido a IA Hoteles</h1>
-      <p>Esta es la API del proyecto conectada a Vercel.</p>
-    </div>
-  );
-}
