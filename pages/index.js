3276d4055d71e86225534b1a853731f5c105c178
viveka2024 <humbertocoaching@gmail.com>
2025-07-02 00:09:25 +0200

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '50px', fontFamily: 'sans-serif' }}>
+      <h1>Bienvenido a IA Hoteles</h1>
+      <p>Esta es la API del proyecto conectada a Vercel.</p>
+    </div>
+  );
+}
